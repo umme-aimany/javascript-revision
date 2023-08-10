a6590a4003fd4d1ad75168ae7948118ba4077a2c
umme aiman <122363026+umme-aimany@users.noreply.github.com>
2023-08-11 00:10:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+alert ("i learn java script")
+var name = "aiman";
+alert(name)
+var age = 23
+alert(age+7)
+
---
+++ java.js
@@ -0,0 +1,14 @@
+alert("i am learning java script");
+
+var  name="haleema";
+alert(name);
+-------------------------------
+var age=23;
+var newage= age-7
+
+alert(9%4)
+---------------------------------------
+var num=0;
+console.log("Number:",num++):
+newNum = ++num;
+console.log("New Number:"newNum)
